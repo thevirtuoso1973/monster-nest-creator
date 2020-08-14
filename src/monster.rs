c9495638394120358a364b3e5b85db660f6f1546
thevirtuoso1973 <chris.tomy@hotmail.com>
2020-08-14 19:37:47 +0100

    add human struct

--- src/monster.rs
+++ src/monster.rs
@@ -1,6 +1,7 @@
 use crate::monster_build::{Head, Body, Arms, Legs};
+use ggez::graphics::Image;
 
-struct Monster {
+pub struct Monster {
     head: Head,
     body: Body,
     arms: Arms,
@@ -11,15 +12,23 @@ impl Monster {
     fn new(head: Head, body: Body, arms: Arms, legs: Legs) -> Self { Self { head, body, arms, legs } }
 }
 
+pub struct Human {
+    sprite_index: usize,
+    tilt: f32,
+    pos: mint::Point2<f32>,
+    speed: f32,
+    range: f32,
+    hp: f32,
+}
 
 pub struct AttackState {
+    human_sprites: Vec<Image>,
     monsters: Vec<Monster>,
+    humans: Vec<Human>
 }
 
 impl AttackState {
-    pub fn new() -> Self {
-        AttackState {monsters: Vec::new()}
-    }
+    pub fn new(human_sprites: Vec<Image>, monsters: Vec<Monster>, humans: Vec<Human>) -> Self { Self { human_sprites, monsters, humans } }
 
     pub fn add_monster(&mut self, head: Head, body: Body, arms: Arms, legs: Legs) {
         self.monsters.push(Monster::new(head, body, arms, legs));
